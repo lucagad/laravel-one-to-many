c21dff17c44cae23fcfa6dfcf9a68762a157788d
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-07-08 14:58:10 +0200

    Added: Category - UpdatePostsTable - CategoriesTableSeeder

---
+++ database/migrations/2022_07_08_114307_update_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+             //creo la colonna della FK
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+
+             //assegno la FK alla colonna creata
+            $table->foreign('category_id')
+            ->references ( 'id')
+            ->on ('categories')
+            ->onDelete( 'set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            
+            // 1. elimino la FK
+            $table->dropForeign(['category_id']);
+            // 2. elimino la colonna
+            $table->dropColumn('category_id');
+
+        });
+    }
+}
---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'HTML',
+            'PHP',
+            'Javascript',
+            'Vue.Js',
+            'CSS',
+            'Laravel'
+        ];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category, '-');
+            $new_category->save();
+
+
+        }
+
+
+    }
+}
